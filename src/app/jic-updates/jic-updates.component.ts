64e285c2d0df873c945f6a04986c71d5984cf60b
AlessiaYChen <alessia.yanchen@gov.bc.ca>
2020-04-15 14:16:19 -0700

    added jic-updates component

---
+++ src/app/jic-updates/jic-updates.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jic-updates',
+  templateUrl: './jic-updates.component.html',
+  styleUrls: ['./jic-updates.component.scss']
+})
+export class JicUpdatesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
